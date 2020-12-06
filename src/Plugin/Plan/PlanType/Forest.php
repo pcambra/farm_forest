ea054d5f9de8a7480bb517feffeed202fdf2a4fc
Michael Stenta <mike@mstenta.net>
2020-12-06 14:04:32 -0500

    Add Forest plan type.

---
+++ src/Plugin/Plan/PlanType/Forest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_forest\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the forest plan type.
+ *
+ * @PlanType(
+ *   id = "forest",
+ *   label = @Translation("Forest"),
+ * )
+ */
+class Forest extends FarmPlanType {
+
+}
