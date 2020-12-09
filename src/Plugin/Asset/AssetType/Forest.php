d845c5ec63770b31943e0b5505bc8e46b21a74c6
Michael Stenta <mike@mstenta.net>
2020-12-09 10:13:44 -0500

    Add Forest asset type.

---
+++ src/Plugin/Asset/AssetType/Forest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_forest\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the forest asset type.
+ *
+ * @AssetType(
+ *   id = "forest",
+ *   label = @Translation("Forest"),
+ * )
+ */
+class Forest extends FarmAssetType {
+
+}
