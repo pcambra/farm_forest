91b973f2b7fa80c45b4dbf5574f5b04c20934fef
Pedro Cambra <pedro@cambra.io>
2020-12-16 18:07:12 +0100

    Set asset field in forest plans as visible

--- src/Plugin/Plan/PlanType/Forest.php
+++ src/Plugin/Plan/PlanType/Forest.php
@@ -14,4 +14,24 @@ use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
  */
 class Forest extends FarmPlanType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+
+    // Assets in the plan.
+    $options = [
+      'type' => 'entity_reference',
+      'label' => $this->t('Assets'),
+      'target_type' => 'asset',
+      'multiple' => TRUE,
+      'hidden' => FALSE,
+    ];
+    $fields['asset'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+    $fields['asset']->setRequired(TRUE);
+
+    return $fields;
+  }
+
 }
